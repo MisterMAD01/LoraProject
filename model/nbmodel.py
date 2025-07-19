fc4b70320d62effcb3612bf573a12929e15efabd
Phat <phasri2547@gmail.com>
2025-07-19 20:41:08 +0700

    update model

--- model/nbmodel.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd 
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.model_selection import cross_val_predict, cross_val_score
-from sklearn.metrics import accuracy_score, classification_report
-
-data = pd.read_csv("./csv/A2.csv")
-data.fillna(0, inplace=True)
-print(data)
